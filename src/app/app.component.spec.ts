9cd157f1126107ba84d75c39471b730adb7cb840
Long Phu <phu.huynh@monimedia.com>
2019-10-09 18:12:52 +0700

    Replace full-site to angular-netlify

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'full-site'`, () => {
+  it(`should have as title 'angular-netlify'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('full-site');
+    expect(app.title).toEqual('angular-netlify');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('full-site app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-netlify app is running!');
   });
 });
